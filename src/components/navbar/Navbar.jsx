fbd3b2c6a02a84c854d2da31f11fe03fd17fbaa8
Goodness Obi <gobi@softalliance.com>
2024-03-09 22:48:02 +0100

    feat: set up loading state and navbar and footer component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Links from './links/Links';
+
+const Navbar = () => {
+	return (
+		<div>
+			<div>Logo</div>
+			<Links />
+		</div>
+	);
+};
+
+export default Navbar;
